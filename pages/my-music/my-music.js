83f135f0f3301df49533d493f9ffd00bc2dbfd57
Sioxas <sioxas@qq.com>
2016-09-29 00:23:02 +0800

    删除了多余图片

--- pages/my-music/my-music.js
+++ pages/my-music/my-music.js
@@ -24,19 +24,19 @@ var _songsList=[{
 var _albumList = [{
       name:'寂寞不痛',
       singer:'A-Lin',
-      image:'a-lin.png'
+      image:'http://y.gtimg.cn/music/photo_new/T002R300x300M000000Nlo922ahOEE.jpg?max_age=2592000'
     },{
       name:'喜剧之王',
       singer:'李荣浩',
-      image:'ronghao-li.png'
+      image:'http://y.gtimg.cn/music/photo_new/T002R300x300M000001FOctH2oGoAx.jpg?max_age=2592000'
     },{
       name:'I Know You Were Trouble.-Single',
       singer:'Taylor Swift',
-      image:'taylor-swift.png'
+      image:'http://y.gtimg.cn/music/photo_new/T002R300x300M000000XafSm26FA1L.jpg?max_age=2592000'
     },{
       name:'哎呦，不错哦',
       singer:'周杰伦',
-      image:'chou-jay.png'
+      image:'http://y.gtimg.cn/music/photo_new/T002R300x300M000001uqejs3d6EID.jpg?max_age=2592000'
     }]
 
 var pageObject = {
