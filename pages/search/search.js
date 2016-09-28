3462e9fb6945be4f1e973670273591edd0b00657
Sioxas <sioxas@qq.com>
2016-09-28 13:44:20 +0800

    完成了音乐搜索功能
    搜索出来的音乐还不能播放...

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,38 @@
-Page({
-    
-})
+var pageObject = {
+        data:{
+
+        },
+        search:function(e){
+            var that = this
+            console.log('搜索')
+            wx.request({
+                url: 'http://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg',
+                data: {
+                    is_xml:0,
+                    format:'jsonp',
+                    key:e.detail.value,
+                    g_tk:5381,
+                    jsonpCallback:'SmartboxKeysCallbackmod_top_search463',
+                    loginUin:0,
+                    hostUin:0,
+                    format:'jsonp',
+                    inCharset:'utf8',
+                    outCharset:'utf-8',
+                    notice:0,
+                    platform:'yqq',
+                    needNewCode:0,
+                },
+                header: {
+                    'Content-Type': 'text/html;charset=utf-8'
+                },
+                success: function(res) {
+                    // var pattern =new RegExp("\\((.*?)\\)","igm");
+                    that.setData({
+                        searchRes:JSON.parse(res.data.substring(38,res.data.length-1)).data
+                    })
+                }
+            })
+        }
+}
+
+Page(pageObject)
