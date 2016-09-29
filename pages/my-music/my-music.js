b456a6427e55e7e534a09a2d82621c2c512faa8b
Sioxas <sioxas@qq.com>
2016-09-29 13:19:27 +0800

    修复了bug

--- pages/my-music/my-music.js
+++ pages/my-music/my-music.js
@@ -3,21 +3,25 @@ var items = ['播放列表', '歌曲', '专辑', '演唱者']
 var _songsList=[{
       dataUrl:'http://stream.qqmusic.tc.qq.com/137192078.mp3',
       name: '告白气球',
+      mid: "003OUlho2HcRHC",
       singer:'周杰伦',
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RMaRI1iFoYd.jpg'
     },{
       dataUrl:'http://stream.qqmusic.tc.qq.com/138549169.mp3',
       name: '你还要我怎样',
+      mid: "000E62Tc3bMiJB",
       singer:'薛之谦',
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000000aWdOx24i3dG.jpg'
     },{
       dataUrl:'http://stream.qqmusic.tc.qq.com/137903929.mp3',
       name: '微微一笑很倾城',
+      mid: "002NbtFA3fuJhD",
       singer:'杨洋',
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RxTdZ0sJLwo.jpg'
     },{
       dataUrl:'http://stream.qqmusic.tc.qq.com/132636799.mp3',
       name: '演员',
+      mid: "001Qu4I30eVFYb",
       singer:'薛之谦',
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003y8dsH2wBHlo.jpg'
     }]
@@ -51,7 +55,6 @@ var pageObject = {
       dataUrl:'http://stream.qqmusic.tc.qq.com/137192078.mp3',
       name: '告白气球',
       singer:'周杰伦',
-      id:137192078,
       coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R90x90M000003RMaRI1iFoYd.jpg'
     },
     songsList:_songsList,
@@ -82,6 +85,7 @@ var pageObject = {
   play: function (event) {
     var that = this
     var res=that.data.songsList[event.currentTarget.dataset.num]
+    getApp().globalData.playing = res
     that.setData({
           playBar:res,
           playingSongsNum:event.currentTarget.dataset.num
@@ -97,7 +101,6 @@ var pageObject = {
         })
       }
     })
-    this._enableInterval()
   },
   pause: function () {
     var that = this
@@ -108,21 +111,6 @@ var pageObject = {
         })
       }
     })
-  },
-  _enableInterval: function () {
-    var that = this
-    update()
-    this.updateInterval = setInterval(update, 500)
-    function update() {
-      wx.getBackgroundAudioPlayerState({
-        success: function (res) {
-          that.setData({
-            playTime: res.currentPosition,
-            formatedPlayTime: util.formatTime(res.currentPosition + 1)
-          })
-        }
-      })
-    }
   },
   onUnload: function () {
     clearInterval(this.updateInterval)
